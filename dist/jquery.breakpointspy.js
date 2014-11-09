f97e9c61753e4253fe7d5696dd364042985a55a5
Casper Kuijjer <casper@kuijjer.com>
2014-11-09 11:11:29 +0100

    Added dist versions

---
+++ dist/jquery.breakpointspy.js
@@ -0,0 +1,8 @@
+/*
+ *  jQuery Breakpoint Spy - v0.0.1
+ *  A plugin that moves content when a Bootstrap breakpoint change occurs
+ *  https://github.com/ckuijjer/jquery-breakpointspy/
+ *
+ *  Made by Casper Kuijjer
+ *  Under MIT License
+ */
