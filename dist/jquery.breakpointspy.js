84f4e64e273bdbe54dfe45b5baa411fd14f154ab
Casper Kuijjer <casper@kuijjer.com>
2014-11-10 21:10:31 +0100

    First implementation

--- dist/jquery.breakpointspy.js
+++ dist/jquery.breakpointspy.js
@@ -6,3 +6,131 @@
  *  Made by Casper Kuijjer
  *  Under MIT License
  */
+;(function ($, window, document, undefined) {
+		var pluginName = "breakpointSpy",
+			defaults = {},
+			breakpoints = ["xs","sm","md","lg"],
+			BreakpointDetector;
+
+		BreakpointDetector = (function() {
+			var initialized = false,
+				detector,
+				current,
+				previous;
+
+			function createDetector() {
+				detector = $("<div />");
+
+				$(breakpoints).each(function() {
+					var className = "visible-" + this + "-block";
+
+					detector.append($("<div />")
+						.addClass(className));
+				});
+
+				$("body").append(detector);
+			}
+
+			function resize() {
+				current = getSize();
+				if (current !== previous) {
+					previous = current;
+
+					detector.trigger("change.breakpointDetector", current);
+				}
+			}
+
+			function getSize() {
+				var el = detector.find(":visible"),
+					className = el.attr("class"),
+					size = className.split("-")[1];
+
+				return size;
+			}
+
+			return {
+				init: function() {
+					createDetector();
+
+					$(window).resize(resize);
+					previous = current = getSize();
+
+					initialized = true;
+				},
+
+				getSize: function() {
+					return getSize();
+				}
+			};
+		})();
+
+		// The actual plugin constructor
+		function BreakpointSpy(element, options) {
+			this.element = element;
+			this.$element = $(element);
+			this.options = $.extend({}, defaults, options, this._getDataAttributes());
+			this._defaults = defaults;
+			this._name = pluginName;
+			this.init();
+		}
+
+		BreakpointSpy.prototype = {
+			init: function () {
+				BreakpointDetector.init();
+
+				// add a event listener to the document as the custom events bubbles up
+				// todo: make sure that as the element is removed this event handler is also
+				// removed.
+				$(document).on("change.breakpointDetector", $.proxy(this._move, this));
+
+				// do the first one manually
+				this._move(null, BreakpointDetector.getSize());
+			},
+
+			_move: function(e, size) {
+				// walk through the breakpoints
+				// xs sm and input lg should return sm
+				// this should be cached yada yada
+				var self = this,
+					selector;
+
+				$(breakpoints).each(function(i, current) {
+					// if a selector is set for the current breakpoint remember it
+					if (self.options[current]) {
+						selector = self.options[current];
+					}
+
+					// exit with looking for the size
+					if (current === size) {
+						return false;
+					}
+				});
+
+				if (selector) {
+					// todo: check if it's already there (or does jQuery handle that?)
+					self.$element.appendTo($(selector));
+				}
+			},
+
+			_getDataAttributes: function() {
+				var self = this,
+					dataAttributes = {};
+
+				$(breakpoints).each(function(i, size) {
+					dataAttributes[size] = self.$element.data("target-" + size);
+				});
+				return dataAttributes;
+			}
+		};
+
+		// A really lightweight plugin wrapper around the constructor,
+		// preventing against multiple instantiations
+		$.fn[ pluginName ] = function ( options ) {
+				return this.each(function() {
+						if ( !$.data( this, "plugin_" + pluginName ) ) {
+								$.data( this, "plugin_" + pluginName, new BreakpointSpy(this, options) );
+						}
+				});
+		};
+
+})(jQuery, window, document);
--- src/jquery.breakpointspy.js
+++ src/jquery.breakpointspy.js
@@ -1,48 +1,118 @@
-// the semi-colon before function invocation is a safety net against concatenated
-// scripts and/or other plugins which may not be closed properly.
-;(function ( $, window, document, undefined ) {
+;(function ($, window, document, undefined) {
+		var pluginName = "breakpointSpy",
+			defaults = {},
+			breakpoints = ["xs","sm","md","lg"],
+			BreakpointDetector;
 
-		// undefined is used here as the undefined global variable in ECMAScript 3 is
-		// mutable (ie. it can be changed by someone else). undefined isn't really being
-		// passed in so we can ensure the value of it is truly undefined. In ES5, undefined
-		// can no longer be modified.
+		BreakpointDetector = (function() {
+			var initialized = false,
+				detector,
+				current,
+				previous;
 
-		// window and document are passed through as local variable rather than global
-		// as this (slightly) quickens the resolution process and can be more efficiently
-		// minified (especially when both are regularly referenced in your plugin).
+			function createDetector() {
+				detector = $("<div />");
 
-		// Create the defaults once
-		var pluginName = "defaultPluginName",
-				defaults = {
-				propertyName: "value"
-		};
+				$(breakpoints).each(function() {
+					var className = "visible-" + this + "-block";
+
+					detector.append($("<div />")
+						.addClass(className));
+				});
+
+				$("body").append(detector);
+			}
+
+			function resize() {
+				current = getSize();
+				if (current !== previous) {
+					previous = current;
+
+					detector.trigger("change.breakpointDetector", current);
+				}
+			}
+
+			function getSize() {
+				var el = detector.find(":visible"),
+					className = el.attr("class"),
+					size = className.split("-")[1];
+
+				return size;
+			}
+
+			return {
+				init: function() {
+					createDetector();
+
+					$(window).resize(resize);
+					previous = current = getSize();
+
+					initialized = true;
+				},
+
+				getSize: function() {
+					return getSize();
+				}
+			};
+		})();
 
 		// The actual plugin constructor
-		function Plugin ( element, options ) {
-				this.element = element;
-				// jQuery has an extend method which merges the contents of two or
-				// more objects, storing the result in the first object. The first object
-				// is generally empty as we don't want to alter the default options for
-				// future instances of the plugin
-				this.options = $.extend( {}, defaults, options );
-				this._defaults = defaults;
-				this._name = pluginName;
-				this.init();
+		function BreakpointSpy(element, options) {
+			this.element = element;
+			this.$element = $(element);
+			this.options = $.extend({}, defaults, options, this._getDataAttributes());
+			this._defaults = defaults;
+			this._name = pluginName;
+			this.init();
 		}
 
-		Plugin.prototype = {
-				init: function () {
-						// Place initialization logic here
-						// You already have access to the DOM element and
-						// the options via the instance, e.g. this.element
-						// and this.options
-						// you can add more functions like the one below and
-						// call them like so: this.yourOtherFunction(this.element, this.options).
-						console.log("xD");
-				},
-				yourOtherFunction: function () {
-						// some logic
+		BreakpointSpy.prototype = {
+			init: function () {
+				BreakpointDetector.init();
+
+				// add a event listener to the document as the custom events bubbles up
+				// todo: make sure that as the element is removed this event handler is also
+				// removed.
+				$(document).on("change.breakpointDetector", $.proxy(this._move, this));
+
+				// do the first one manually
+				this._move(null, BreakpointDetector.getSize());
+			},
+
+			_move: function(e, size) {
+				// walk through the breakpoints
+				// xs sm and input lg should return sm
+				// this should be cached yada yada
+				var self = this,
+					selector;
+
+				$(breakpoints).each(function(i, current) {
+					// if a selector is set for the current breakpoint remember it
+					if (self.options[current]) {
+						selector = self.options[current];
+					}
+
+					// exit with looking for the size
+					if (current === size) {
+						return false;
+					}
+				});
+
+				if (selector) {
+					// todo: check if it's already there (or does jQuery handle that?)
+					self.$element.appendTo($(selector));
 				}
+			},
+
+			_getDataAttributes: function() {
+				var self = this,
+					dataAttributes = {};
+
+				$(breakpoints).each(function(i, size) {
+					dataAttributes[size] = self.$element.data("target-" + size);
+				});
+				return dataAttributes;
+			}
 		};
 
 		// A really lightweight plugin wrapper around the constructor,
@@ -50,9 +120,9 @@
 		$.fn[ pluginName ] = function ( options ) {
 				return this.each(function() {
 						if ( !$.data( this, "plugin_" + pluginName ) ) {
-								$.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
+								$.data( this, "plugin_" + pluginName, new BreakpointSpy(this, options) );
 						}
 				});
 		};
 
-})( jQuery, window, document );
+})(jQuery, window, document);
